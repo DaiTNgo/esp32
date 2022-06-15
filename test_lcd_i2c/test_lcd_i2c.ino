35e8b9308bc996f06327a7072fd523bc4b49ec8a
daingo_linux <daingo7211@gmail.com>
2022-06-15 15:36:08 +0700

    abc

--- test_lcd_i2c/test_lcd_i2c.ino
+++ test_lcd_i2c/test_lcd_i2c.ino
@@ -5,9 +5,9 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
   lcd.init();
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.backlight();
-//  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 0);
   lcd.print("Line 1 test OK");
   lcd.setCursor(0, 1);
   lcd.print("Line 2 test OK");
